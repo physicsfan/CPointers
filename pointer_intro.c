75a3dda00ecbbebbb6c19c579bf55a783de30b18
physicsfan <asenchuk@hotmail.com>
2019-05-09 21:21:12 -0500

    The first file exploring C pointers

---
+++ pointer_intro.c
@@ -0,0 +1,18 @@
+/* pointer_intro.c */
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+  /* declare a ointer variable to hold an address of
+     an integer.  Do not worry about understanding the
+     exact syntax, we will cover this in more detail in 
+     chapter 3. */
+  int *integer_pointer;
+
+  /* declare an integer variable.  This is a continer
+     that holds an integer. */
+  int myint;
+
+  /* assign an integer to the variable "my_int" */
+  myint = 10;
