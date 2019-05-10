23b17fdf40faeeb45db3961eceaed6d642abebeb
physicsfan <asenchuk@hotmail.com>
2019-05-09 21:48:40 -0500

    finished code

--- pointer_intro.c
+++ pointer_intro.c
@@ -1,18 +1,36 @@
 /* pointer_intro.c */
+/*
+  Synopsis:
+  The program declares a pointer variable to hold an 
+  address of an integer. Then it declares an integer variable 
+  "my_int", which is a continer that holds an integer.  After 
+  assigning the integer to the variable, the program prints out 
+  the address of the integer.  This is the unique memory address 
+  where the integer is stored.  Since a pointer can store the 
+  address of another container, we can put the address of integer 
+  variable into the integer pointer.
+*/
 #include <stdio.h>
 #include <stdlib.h>
 
 int main(void)
-{
-  /* declare a ointer variable to hold an address of
-     an integer.  Do not worry about understanding the
-     exact syntax, we will cover this in more detail in 
-     chapter 3. */
+{ 
+  /* declare pointers and variables */
   int *integer_pointer;
-
-  /* declare an integer variable.  This is a continer
-     that holds an integer. */
   int myint;
 
-  /* assign an integer to the variable "my_int" */
-  myint = 10;
+  /* assign my_int */
+  myint = 100;
+
+  /* print address */
+  printf("The address of myint is %p.\n", &myint);
+
+  /* assign address of myint to integer_pointer */
+  integer_pointer = &myint;
+
+  /* print address held in pointer */
+  printf("The integer_pointer holds the address %p.\n",
+	 integer_pointer);
+
+  return EXIT_SUCCESS;
+}
